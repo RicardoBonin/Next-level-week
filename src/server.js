cbbe5d9cce3b085b47e97a09ef1928a32f04fa23
Ricardo Bonin <ricardo@brittes.com.br>
2020-06-18 23:49:01 -0300

    Configurando o Servidor express nodemon nunjucks

--- src/server.js
+++ src/server.js
@@ -1,5 +1,30 @@
 const express = require("express");
+const e = require("express");
 const server = express();
+// configurar pasta public
+server.use(express.static("public"));
+
+//utilizando templete engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true,
+});
+//configurar caminhos da minha aplicação
+// página inicial
+// req: Requisição
+// res: Resposta
+server.get("/", (req, res) => {
+  return res.render("index.html", { title: "um titulo" });
+});
+
+server.get("/create-point", (req, res) => {
+  return res.render("create-point.html");
+});
+
+server.get("/search", (req, res) => {
+  return res.render("search-results.html");
+});
 
 //ligar o servidor
 server.listen(3000);

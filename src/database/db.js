6517eb51587aad9f6a7b9677562b3741da4bac5e
Ricardo <ricardo@brittes.com.br>
2020-06-21 15:11:54 -0300

    Adicionando tabela campos e valores ao db

--- src/database/db.js
+++ src/database/db.js
@@ -6,5 +6,46 @@ const db = new sqlite3.Database("./src/database/database.db");
 
 // utilizar o objeto de banco de dados, para nossas operações
 db.serialize(() => {
-  //Criar um tabela
+  //com comnados SQL eu vou:
+
+  //1Criar um tabela
+  db.run(`
+    CREATE TABLE IF NOT EXISTS pleces (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      image TEXT,
+      name TEXT,
+      anddress TEXT,
+      anddress2 TEXT,
+      state TEXT,
+      city TEXT,
+      items TEXT
+    );
+  `)
+
+  //2Inserir dados na tabela
+  const query = `
+  INSERT INTO places (
+    image,
+    name,
+    anddress,
+    andress2,
+    state,
+    city,
+    items
+  ) VALUES (?,?,?,?,?,?,?);
+`
+const values = [
+  "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+  "Colectoria",
+  "Guilherme Gemballa, Jardim América",
+  "Nº 260",
+  "Santa Catarina",
+  "Rio do Sul",
+  "Resíduos Eletrônicos, Lâmpadas"
+]
+  db.run(query, values, function() {})
+
+  //3Consultar os dados da tabela
+
+  //4Deletar  um dado da tabela
 });
